7fb208437c8bbfa035692434d1426053bf48527a
Sheikh <sksaad48@gmail.com>
2021-03-10 11:16:43 +0600

    update

--- shlick.sh
+++ shlick.sh
@@ -3,47 +3,47 @@
 # DEPENDENCY CHECK
 deps="pandoc"
 for dep in $deps; do
-  command -v "$1" 1>/dev/null || {
-    printf "$1 not found. Please install it.\n";
-    exit 2;
-  }
+    command -v "$1" 1>/dev/null || {
+        printf "$1 not found. Please install it.\n";
+        exit 2;
+    }
 done
 
 # FILE CONVERSION
 convert() {
-  SITE_PATH=$(pwd)
+    SITE_PATH=$(pwd)
 
-  fullpath=$1
-  dirpath=$( dirname $1 )
-  sourcefile=$( basename $1 )
-  targetfile="$(find '$SITE_PATH' -wholename '$SITE_PATH/*.md' -exec sh -c 'basename {} .md' \;).html"
+    fullpath=$1
+    dirpath=$( dirname $1 )
+    sourcefile=$( basename $1 )
+    targetfile="$(find '$SITE_PATH' -wholename '$SITE_PATH/*.md' -exec sh -c 'basename {} .md' \;).html"
 
-  # Use git to get creation and modification date
-  lastUpdated=$( git log -1 --format="%ci" -- $fullpath | cut -f 1 -d ' ' )
-  creationDate=$( git log --format="%ai" -- $fullpath | tail -1 | cut -f 1 -d ' ' )
+    # Use git to get creation and modification date
+    lastUpdated=$( git log -1 --format="%ci" -- $fullpath | cut -f 1 -d ' ' )
+    creationDate=$( git log --format="%ai" -- $fullpath | tail -1 | cut -f 1 -d ' ' )
 
-  echo Processing: $fullpath
+    echo Processing: $fullpath
 
-  rm $dirpath/$targetfile
-  pandoc $fullpath \
-    -o $dirpath/$targetfile \
-    --standalone \
-    --css "/assets/css/style.css" \
-    --css "/assets/css/custom.css" \
-    --template=$SITE_PATH/template/template.html \
-    --variable=lastUpdated:$lastUpdated \
-    --variable=creationDate:$creationDate ;
+    rm $dirpath/$targetfile
+    pandoc $fullpath \
+        -o $dirpath/$targetfile \
+        --standalone \
+        --css "/assets/css/style.css" \
+        --css "/assets/css/custom.css" \
+        --template=$SITE_PATH/template/template.html \
+        --variable=lastUpdated:$lastUpdated \
+        --variable=creationDate:$creationDate ;
 
-  return
+    return
 }
 
 # recursively convert all .md files in current directory
 main() {
-  SITE_PATH=$(pwd)
+    SITE_PATH=$(pwd)
 
-  find $SITE_PATH -name *.md -exec convert {} \;
+    find $SITE_PATH -name *.md -exec convert {} \;
 
-  exit 0;
+    exit 0;
 }
 
 main
